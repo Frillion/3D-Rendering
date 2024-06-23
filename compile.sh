00f9f7390f90d5647377cf0a4fd8a5ff4890fcd9
Frillion <fridriksoebech@gmail.com>
2024-06-23 00:38:55 +0000

    Finally wrote something kindof cool today, the first vertex and fragment shaders as well as actually rendering a triangle to the screen

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ src/Application.cpp -m32 -lopengl32 -lglew32 -lglfw3dll -o render.exe
--- src/Application.cpp
+++ src/Application.cpp
@@ -2,9 +2,45 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int CompileShader(const std::string& source, unsigned int type){
+    unsigned int id = glCreateShader(type);
+    const char* src = &source[0];
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
 
-int main(void)
-{
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE){
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char * message = (char *)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader: " << id << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader){
+    unsigned int program = glCreateProgram();
+    unsigned int vertex_shader = CompileShader(vertexShader,GL_VERTEX_SHADER);
+    unsigned int fragment_shader = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return program;
+}
+
+int main(void){
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -14,8 +50,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window){
         glfwTerminate();
         return -1;
     }
@@ -29,23 +64,49 @@ int main(void)
     
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    unsigned int buffer_id;
-    glGenBuffers(1, &buffer_id);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    float triangle_coordinates[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f,-0.5f
+    };
 
+    unsigned int triangle_buffer;
+    glGenBuffers(1, &triangle_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, triangle_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, triangle_coordinates, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glEnableVertexAttribArray(0);
+
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main(){\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main(){\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)){
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
