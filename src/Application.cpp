0aa906c8cace1cbabdbf73dfae3a1b7730997908
Frillion <fridriksoebech@gmail.com>
2024-07-12 14:31:43 +0000

    Refactored all of the code regarding shader parsing/creating into a Shader class

--- src/Application.cpp
+++ src/Application.cpp
@@ -81,10 +81,9 @@ int main(void){
     float increment = 0.05f;
 
     GLCall(glBindVertexArray(0));
-    GLCall(glUseProgram(0));
+    shader->UnBind();
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
+    index_buffer->Unbind();
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)){
         /* Render here */
